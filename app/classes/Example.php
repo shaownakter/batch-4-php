9aa480ce079cfb24e9056fa0153f53ea2aab6a30
unknown <shaownakter.1995@gmail.com>
2022-02-03 17:20:41 +0600

    file upload

---
+++ app/classes/Example.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace  App\classes;
+class Example
+{
+    public $firstNumber=10;
+   public $firstName;
+   public $lastName;
+
+public function index()
+{
+   /* echo "hello world";*/
+    $this->firstName = "Shaown";
+    $this->lastName = "Akter";
+    echo $this->firstNumber;
+    echo '<br/>';
+    echo $this->firstName;
+    echo '<br/>';
+    echo $this->lastName;
+    echo '<br/>';
+    echo $this->firstName.$this->lastName;
+
+    /*echo $this->firstNumber;*/
+
+}
+}
